bfc28bd61554c13a8ffcacbe8f4ef468ebc3ba25
zyx-0314 <ramirezian037@gmail.com>
2024-08-19 08:45:27 +0800

    Initial Commit

---
+++ Responsive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Responsive
+//
+//  Created by Mac User on 8/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
