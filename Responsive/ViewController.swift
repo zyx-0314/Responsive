a7e7a6442cd8fc5fbfa62f9da04b4cf42fed581d
zyx-0314 <ramirezian037@gmail.com>
2024-08-19 09:44:30 +0800

    feat: practice

--- Responsive/ViewController.swift
+++ Responsive/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func btnResetAction(_ sender: Any) {
+        // creating alert
+        let alert = UIAlertController(title: "Reset", message: "Are you sure you want to reset?", preferredStyle: .alert)
+        
+        // adding buttons on alert
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler:{UIAlertAction in   self.txtPassword.text = ""
+            self.txtUsername.text = ""
+            self.txtEmail.text = ""
+            self.txtConfirmPassword.text = ""
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+        
+        self.present(alert, animated: true)
+    }
+    
 
 }
 
